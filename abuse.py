f1ce5ab70799e0977d2cdac98f717ec03b57029f
timosarkar <timosarkartimo@gmail.com>
2025-09-15 13:06:11 +0200

    redone abuse.ch scirpt

--- abuse.py
+++ abuse.py
@@ -1,9 +1,10 @@
 # script to get recent detections from abuse.ch API for threat intelligence
 import requests
 import os
-url = "https://mb-api.abuse.ch/api/v1/"
 
+url = "https://mb-api.abuse.ch/api/v1/"
 ABUSE_API_KEY = os.getenv("ABUSE_API_KEY")
+
 headers = {
     "Auth-Key": f"{ABUSE_API_KEY}"
 }
@@ -11,5 +12,9 @@ data = {
     "query": "recent_detections"
 }
 
-response = requests.post(url, headers=headers, data=data)
-print(response.text)
+response = requests.post(url, headers=headers, data=data).json()
+
+for item in response.get("data", []):
+    signature = item.get("signature", "N/A")
+    sha256 = item.get("sha256_hash", "N/A")
+    print(f"Signature: {signature}, SHA256: {sha256}")
